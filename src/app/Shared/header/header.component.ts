3ff85253b134eafb582b4ddac119020370eaf126
Cadu Bifulco Torres <cadubt@gmail.com>
2021-12-23 14:44:23 -0300

    Including another kind of login validation on TS

--- src/app/Shared/header/header.component.ts
+++ src/app/Shared/header/header.component.ts
@@ -14,18 +14,30 @@ export class HeaderComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    if(sessionStorage.getItem("token") == null){
+    if(
+    (sessionStorage.getItem("token") == null) ||
+    (sessionStorage.getItem("userId") == null) ||
+    (sessionStorage.getItem("userName") == null) ||
+    (sessionStorage.getItem("userRole") == null) ||
+    (sessionStorage.getItem("userEmail") == null) ||
+    (sessionStorage.getItem("userDeleted") != "null")){
       this.router.navigate(['/'])
+      alert(sessionStorage.getItem("token") + " | " +
+      sessionStorage.getItem("userId") + " | " +
+      sessionStorage.getItem("userName") + " | " +
+      sessionStorage.getItem("userRole") + " | " +
+      sessionStorage.getItem("userEmail") + " | " +
+      sessionStorage.getItem("userDeleted"))
     }
   }
 
   backToLogin() {
     sessionStorage.removeItem('token');
-    sessionStorage.removeItem('userId')
-    sessionStorage.removeItem('userName')
-    sessionStorage.removeItem('userRole')
-    sessionStorage.removeItem('userEmail')
-    sessionStorage.removeItem('userDeleted')
+    sessionStorage.removeItem('userId');
+    sessionStorage.removeItem('userName');
+    sessionStorage.removeItem('userRole');
+    sessionStorage.removeItem('userEmail');
+    sessionStorage.removeItem('userDeleted');
     return this.router.navigate(['/'])
   }
 
